720f9931d40e73b405badfe9198057c0d718d46b
joshua.ewer <joshua.ewer@dowjones.com>
2014-10-03 15:15:06 -0500

    oops.  should actually test this

--- scala/fizzbuzz.scala
+++ scala/fizzbuzz.scala
@@ -1,3 +1,3 @@
 val a = Map(3 -> "Fizz", 5 -> "Buzz")
 
-(1 to 100).map(i => x.collect{case e @ (_:Int, _:String) if i % e._1 == 0 => e._2} match {case Nil=> List(i.toString) case l=> l}).foreach(x => println(x.mkString))
+(1 to 100).map(i => a.collect{case e @ (_:Int, _:String) if i % e._1 == 0 => e._2} match {case Nil=> List(i.toString) case l=> l}).foreach(x => println(x.mkString))
