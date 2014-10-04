84039cb1b79d0314b114aaffb48a2ba63c1fac48
Conrad Dean <conrad.p.dean@gmail.com>
2014-10-04 12:02:38 -0500

    just indentation changes for pep8

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -1,24 +1,24 @@
 def advanced(myDict):
-  found = False
-  line = ''
-  keys = myDict.keys()
-  cleanKeys = []
-  for key in keys:
-    cleanKeys.append(int(key))
-  for i in range(1, 101):
     found = False
     line = ''
-    for j in range(0, len(cleanKeys)):
-      if i % cleanKeys[j] == 0:
-        found = True
-        line += myDict[keys[j]]
-    if not found:
-      line += str(i)
-    print str(i) + ': ' + line
+    keys = myDict.keys()
+    cleanKeys = []
+    for key in keys:
+        cleanKeys.append(int(key))
+    for i in range(1, 101):
+        found = False
+        line = ''
+        for j in range(0, len(cleanKeys)):
+            if i % cleanKeys[j] == 0:
+                found = True
+                line += myDict[keys[j]]
+        if not found:
+            line += str(i)
+        print str(i) + ': ' + line
 
 advanced({
-  '2': 'second',
-  '3': 'Fizz',
-  '5': 'Buzz',
-  '6': 'sixth'
+    '2': 'second',
+    '3': 'Fizz',
+    '5': 'Buzz',
+    '6': 'sixth'
 })
