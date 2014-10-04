096efc6d4816112b2f7784f3f657811d87790da1
Conrad Dean <conrad.p.dean@gmail.com>
2014-10-04 12:20:43 -0500

    rename variables to be a little more descriptive, remove side effects for testing

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -1,22 +1,24 @@
-def advanced(myDict):
+def advanced(divisor_mappings):
+    output = []
     found = False
     line = ''
-    keys = myDict.keys()
-    cleanKeys = []
+    keys = divisor_mappings.keys()
+    divisors = []
     for key in keys:
-        cleanKeys.append(int(key))
+        divisors.append(int(key))
     for i in range(1, 101):
         found = False
         line = ''
-        for j in range(0, len(cleanKeys)):
-            if i % cleanKeys[j] == 0:
+        for j in range(0, len(divisors)):
+            if i % divisors[j] == 0:
                 found = True
-                line += myDict[keys[j]]
+                line += divisor_mappings[keys[j]]
         if not found:
             line += str(i)
-        print str(i) + ': ' + line
+        output.append(str(i) + ': ' + line)
+    return "\n".join(output)
 
-advanced({
+print advanced({
     '2': 'second',
     '3': 'Fizz',
     '5': 'Buzz',
