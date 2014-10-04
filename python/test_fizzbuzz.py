bb6eb6e440647ebc97b68cf7845e185bc8dde4d4
Conrad Dean <conrad.p.dean@gmail.com>
2014-10-04 13:19:06 -0500

    since we're being educational

--- python/test_fizzbuzz.py
+++ python/test_fizzbuzz.py
@@ -8,11 +8,21 @@ def test_fizzbuzz():
         '5': 'Buzz',
         '6': 'sixth'
     }
+    """
+    context managers are a pretty sweet python feature,
+    the block below will close the file for us even if python
+    crashes. it replaces the try/catch/finally boilerplate in other
+    languages, though you could still do that if you wanted.
+
+    context managers work with database connections, sockets, thread locks
+    and anything else that implements the interface
+    """
     with open("desired_output.txt") as f:
         output = fizzbuzz.advanced(config)
         tests = [i.strip() for i in f.readlines()]
         for expected, actual in zip(tests, output.split("\n")):
             assert expected == actual, (expected, actual)
+
     print "OK"
 
 
